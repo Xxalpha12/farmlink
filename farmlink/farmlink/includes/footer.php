b1dd3de21fb5c1269a22ff63a0e889f9c3eeac2f
Xxalpha12 <herobeatrice36@gmail.com>
2026-09-21 15:57:39 +0100

    Delete farmlink/farmlink directory

--- farmlink/farmlink/includes/footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-<footer class="site-footer">
-  <p>&copy; <?= date('Y') ?> Farm Link &mdash; Linking Farmers to Buyers. Final Year Project System.</p>
-</footer>
-<script src="/js/script.js"></script>
-</body>
-</html>
